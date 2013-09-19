39ea376019ce5f67fd6f55ebc75de5b74b772c1d
Jed Verity <jed@dataverse.me>
2013-09-19 15:18:55 -0400

    removing old code

--- csc_isotope/plugins/content_types/csc_isotope.filter.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  $(document).ready(function () {
-    $('#filters a').click(function(){
-      var selector = $(this).attr('data-filter');
-      $('#iso-container').isotope({ filter: selector });
-      return false;
-    });
-  });
-})(jQuery);
