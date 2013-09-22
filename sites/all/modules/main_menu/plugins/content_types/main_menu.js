73ec20888b0472c2dcb12b78224ce611862ec3f6
alextrejo <trejo_alexander@yahoo.com>
2013-09-22 14:51:15 -0430

    Auto expand sub-menu
    You can test it on sample site with CONTEXT menu items

--- sites/all/modules/main_menu/plugins/content_types/main_menu.js
+++ sites/all/modules/main_menu/plugins/content_types/main_menu.js
@@ -1,5 +1,6 @@
 (function ($) {
   $(document).ready(function () {
+    //Bind expand/contract effect to menus
     $('#main-menu > .expanded > a').each(function(){
       $(this).attr('href','#');
       $(this).on("click",function(e){
@@ -15,5 +16,30 @@
         return false;
       });
     });
+
+    //Auto expand for active sub-menus
+    var el = false;
+    var url = String(location);
+    //Search if current page is a sub-menu
+    $('ul.menu > li > a').each(function(index){
+      hr = $(this).attr('href'); console.log(hr);
+      found = url.search(hr);
+      if(found > 0){
+        el = $(this);
+      }  
+    });
+    
+    if(el){
+      var ul = el.parent().parent();  //UL of current anchor
+      //it is a sub-menu
+      if(ul.attr('id') != 'main-menu'){
+        var li = ul.parent();
+        if(li.hasClass('expanded')){
+          li.removeClass('expanded');
+          li.addClass('contracted');
+        }
+      }
+    }    
+    
   });
 })(jQuery);
