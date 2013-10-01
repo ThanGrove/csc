031695faf8c3259f9957aaade075a5b3a2881f35
Jed Verity <jed@dataverse.me>
2013-10-01 15:29:39 -0400

    lightbox improvements

--- sites/all/modules/csc_teaser/plugins/content_types/csc_teaser.js
+++ sites/all/modules/csc_teaser/plugins/content_types/csc_teaser.js
@@ -1,20 +1,16 @@
 (function ($) {
   $(document).ready(function () {
-    var $overlay = $('<div/>', {class: 'overlay', id: 'teaser-overlay'}).appendTo('body');
+    var $overlay = $('<div/>', {class: 'overlay', id: 'teaser-overlay'}).appendTo('body'),
+        $tooltip = $('.tooltip').appendTo('body');
+
     $('.csc-teaser-more').click(function(e) {
       $overlay.fadeIn();
-      $('.tooltip').addClass('in');
-      //$('#csc-teaser-body').lightbox_me({
-        //appearEffect: 'show',
-        //lightboxSpeed: 0,
-        //overlaySpeed: 0,
-        //className: 'in'
-      //});
+      $tooltip.addClass('in');
     });
    
     $overlay.click(function(){
       $(this).fadeOut();
-      $('.tooltip').removeClass('in');
+      $tooltip.removeClass('in');
     });
   });
 })(jQuery);
