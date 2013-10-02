d9095bb3b5b494de8be29c08508cfa4323e4bdd4
Jed Verity <jed@dataverse.me>
2013-10-02 15:23:04 -0400

    events list and secondary page styling

--- sites/all/modules/csc_node_image/plugins/content_types/csc_node_image.js
+++ sites/all/modules/csc_node_image/plugins/content_types/csc_node_image.js
@@ -1,19 +1,6 @@
 (function ($) {
 
 $(window).load(function() {
-    //var ratio = 720/960;
-    
-    //var sw = $('.pane-csc-node-image').width();
-    //var sh = Math.round(sw * ratio);
-    //$('#csc-node-images').bjqs({
-        //'height' : sh,
-        //'width' : sw,
-        ////'responsive' : true,
-        //'automatic' : false,
-        //'showmarkers' : false,
-        //'animtype' : 'slide'
-    //});
-  //
     $('#csc-node-images').find('.rslides').responsiveSlides({
       auto: false,
       nav: true,
