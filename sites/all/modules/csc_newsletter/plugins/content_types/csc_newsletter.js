e50c1a775be0abac807369d3da773b916cb1d688
alextrejo <trejo_alexander@yahoo.com>
2013-12-10 08:23:19 -0430

    Newslatter modal buttons

--- sites/all/modules/csc_newsletter/plugins/content_types/csc_newsletter.js
+++ sites/all/modules/csc_newsletter/plugins/content_types/csc_newsletter.js
@@ -12,25 +12,25 @@
      }
      
      $( "#dialog-modal" ).dialog({
-  		 height: 250,
+  		 height: 270,
        width: 370,
   		 modal: true,
        buttons: {
-        "Close": function() { $(this).dialog("close");},
-        "Accept": function(){
-                    if( $('#news-1').prop('checked') ) 
-                      $('#mce-group-16045-0').prop('checked', true );
-                    else
-                      $('#mce-group-16045-0').prop('checked', false);
-
-                    if( $('#modal-form #news-2').prop('checked') ) 
-                      $('#mce-group-16045-1').prop('checked', true);
-                    else
-                      $('#mce-group-16045-1').prop('checked', false);
-                      
-                    f.submit();
-                    $(this).dialog("close");
-                  }
+        "Cancel":    function() { $(this).dialog("close");},
+        "Subscribe": function(){
+                      if( $('#news-1').prop('checked') ) 
+                        $('#mce-group-16045-0').prop('checked', true );
+                      else
+                        $('#mce-group-16045-0').prop('checked', false);
+  
+                      if( $('#modal-form #news-2').prop('checked') ) 
+                        $('#mce-group-16045-1').prop('checked', true);
+                      else
+                        $('#mce-group-16045-1').prop('checked', false);
+                        
+                      f.submit();
+                      $(this).dialog("close");
+                    }
        }
   	 });
     });
