cef5aeeca8199cb54c46a5193e0e6b821150e565
Jed Verity <jed@dataverse.me>
2014-05-09 13:13:34 -0400

    calendar changes

--- sites/all/themes/csc/js/csc_common.js
+++ sites/all/themes/csc/js/csc_common.js
@@ -1,12 +1,13 @@
 (function($){
     $(window).load(function(){
         $(".add-scroll").mCustomScrollbar();
+        $('#m-3223').find('a')[0].target = '_blank';
         
-      	$("#calendar-link").fancybox({
-      		ajax : {
-      		    type	: "GET"
-      		}
-      	});
+        //$("#calendar-link").fancybox({
+          //ajax : {
+              //type	: "GET"
+          //}
+        //});
 
         //$('.csc-directors-wrapper').on('click', function(){
           //$(this).find('a').click();
